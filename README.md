061cb5907efc2b394e76b30f8479cbe3cbb976bc
Lucas Mendes <lucasdaniel9183@gmail.com>
2022-10-07 15:25:10 -0300

    version 1
    - created the first version of the script
    - created the readme

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # wsl_portproxy
- expose wsl port to your local area network
+ exposes the wsl ports to your local area network
+
+## Usage
+ It's simple, just tell to script what ports you need separeted by comma
+
+ `./proxy_ports -ports 3000,3056`
+
+ You canspecify a distro and an user if you wnat
+
+ `./proxy_ports -distro Ubuntu-20.04 -user lucas -ports 3000`
+
+## Parameters
+
+ | Parameter | type | Optional | Default value  |
+ |-----------|------|----------|----------------|
+ | distro    | string |yes      | default wsl distro |
+ | user      | string |yes      | returns from whoami of linux |
+ | ports     | list of numbers (comma separeted) |no       | 
+
+---
+<br/>
+
+### I hope this can help you
---
+++ proxy_ports.ps1
@@ -0,0 +1,31 @@
+Param(
+    [Parameter(HelpMessage = "WSL distro name")]
+    $distro,
+    [Parameter(HelpMessage = "WSL user name")]
+    $user,
+    [Parameter(Mandatory, HelpMessage = "List of ports to proxy (separeted by ',')")]
+    $ports,
+    [Parameter(HelpMessage = "Delete all proxys for given ports")]
+    $clearOldProxys=$true
+)
+
+if(-Not $PSBoundParameters.ContainsKey('distro')) {
+    $distros = wsl -l
+    $distro = (($distros -split "`n")[2] -split ' ')[0]
+}
+
+if(-Not $PSBoundParameters.ContainsKey('user')) {
+    $user = wsl whoami
+}
+
+$portsArray = [int[]]($ports -split ',')
+$response = wsl ip route get 8.8.8.8
+$wslIp = ($response -split ' ')[2]
+
+
+Write-Host "Creating proxy for user $user on distro $distro whose ip is: $wslIp"
+
+foreach ($port in $portsArray) {
+    Write-Host "Creating proxy to port $port"
+    netsh interface portproxy add v4tov4 listenport=$port listenaddress=0.0.0.0 connectport=$port connectaddress=$wslIp
+}
